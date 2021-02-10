878dba2490e27cedfc195d98aa1f38d6b0fb3d98
Walisson Rodrigo <walisson.rodrigo@turbi.com.br>
2021-02-09 23:44:11 -0300

    refactor controllers. add AuthController with all implementation for auth using JwtToken for login and change password. refactor in user route to simplify and optimize module user route. add routes to authentication and index to import modules and path routes

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,21 +1,51 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  Unique,
+  CreateDateColumn,
+  UpdateDateColumn
+} from "typeorm";
+import { Length, IsNotEmpty } from "class-validator";
+import * as bcrypt from "bcryptjs";
 
 @Entity()
+@Unique(["email"])
 export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Column()
+  @IsNotEmpty()
+  @Length(3, 20)
+  name: string;
 
-    @Column()
-    name: string;
+  @Column()
+  @IsNotEmpty()
+  @Length(4, 20)
+  email: string;
 
-    @Column()
-    email: string;
+  @Column()
+  @Length(6, 100)
+  password: string;
 
-    @Column()
-    password: string;
+  @Column()
+  @IsNotEmpty()
+  role: string;
 
-    @Column()
-    role: string;
+  @Column()
+  @CreateDateColumn()
+  createdAt: Date;
 
+  @Column()
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  hashPassword() {
+    this.password = bcrypt.hashSync(this.password, 8);
+  }
+
+  checkIfUnencryptedPasswordIsValid(unencryptedPassword: string) {
+    return bcrypt.compareSync(unencryptedPassword, this.password);
+  }
 }
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,23 +1,35 @@
-import {UserController} from "../controller/UserController";
+import { Router } from "express";
+import UserController from "../controllers/UserController";
+import { checkJwt } from "../middlewares/checkJwt";
+import { checkRole } from "../middlewares/checkRole";
 
-export const Routes = [{
-    method: "get",
-    route: "/users",
-    controller: UserController,
-    action: "all"
-}, {
-    method: "get",
-    route: "/users/:id",
-    controller: UserController,
-    action: "one"
-}, {
-    method: "post",
-    route: "/users",
-    controller: UserController,
-    action: "save"
-}, {
-    method: "delete",
-    route: "/users/:id",
-    controller: UserController,
-    action: "remove"
-}];
+const router = Router();
+
+// Get all users
+router.get("/", [checkJwt, checkRole(["ADMIN"])], UserController.listAll);
+
+// Get one user
+router.get(
+  "/:id([0-9]+)",
+  [checkJwt, checkRole(["ADMIN"])],
+  UserController.getOneById
+);
+
+// Create a new user
+router.post("/", [checkJwt, checkRole(["ADMIN"])], UserController.newUser);
+
+// Edit one user
+router.patch(
+  "/:id([0-9]+)",
+  [checkJwt, checkRole(["ADMIN"])],
+  UserController.editUser
+);
+
+// Delete one user
+router.delete(
+  "/:id([0-9]+)",
+  [checkJwt, checkRole(["ADMIN"])],
+  UserController.deleteUser
+);
+
+export default router;
