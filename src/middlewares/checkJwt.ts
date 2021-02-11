c09844bd65818c0b6d44d5cc4f7644015519f96d
Walisson Rodrigo <walisson.rodrigo@turbi.com.br>
2021-02-10 23:40:43 -0300

    valition to prefix Bearer in request tokens on middleware

--- src/middlewares/checkJwt.ts
+++ src/middlewares/checkJwt.ts
@@ -2,29 +2,33 @@ import { Request, Response, NextFunction } from "express";
 import * as jwt from "jsonwebtoken";
 import config from "../config/config";
 
-export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
-  //Get the jwt token from the head
-  const token = <string>req.headers["auth"];
-  let jwtPayload;
+export const checkJwt = (req: Request, res: Response, next: NextFunction): void => {
+  // Get the jwt token from the head
+  const token = <string>req.headers.authorization;
+  let jwtPayload: { userId: any; email: any; };
 
-  //Try to validate the token and get data
+  // Try to validate the token and get data
   try {
-    jwtPayload = <any>jwt.verify(token, config.jwtSecret);
+    if (!token.includes('Bearer '))
+      throw new Error('Prefix Bearer Token not present in current token!');
+    jwtPayload = <any>(
+      jwt.verify(token.replace('Bearer ', ''), config.jwtSecret)
+    );
     res.locals.jwtPayload = jwtPayload;
   } catch (error) {
-    //If token is not valid, respond with 401 (unauthorized)
+    // If token is not valid, respond with 401 (unauthorized)
     res.status(401).send();
     return;
   }
 
-  //The token is valid for 1 hour
-  //We want to send a new token on every request
-  const { userId, username } = jwtPayload;
-  const newToken = jwt.sign({ userId, username }, config.jwtSecret, {
+  // The token is valid for 1 hour
+  // We want to send a new token on every request
+  const { userId, email } = jwtPayload;
+  const newToken = jwt.sign({ userId, email }, config.jwtSecret, {
     expiresIn: "1h"
   });
   res.setHeader("token", newToken);
 
-  //Call the next middleware or controller
+  // Call the next middleware or controller
   next();
 };
