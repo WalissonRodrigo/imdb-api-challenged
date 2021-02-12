a1620b173e3ce4f5fee77a8faa77da96009e302f
Walisson Rodrigo <walisson.rodrigo@turbi.com.br>
2021-02-12 10:58:37 -0300

    remove build git to test deploy heroku

--- build/config/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    jwtSecret: "@Teste-Backend-Node-Api_IMDb"
-};
-//# sourceMappingURL=config.js.map
