91715e33f59f43e259fa863e29cbe55b13f65a9d
Walisson Rodrigo <walisson.rodrigo@turbi.com.br>
2021-02-14 17:55:22 -0300

    add route to movie. implement basic rest routes in movie.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import auth from "./auth";
 import user from "./user";
+import movie from './movie';
 
 const routes = Router();
 // response default to not return error on server.
-routes.get("/", function (req, res) {
-  return res.send("Welcome in the API IMDb Teste Backend");
+routes.get("/", [], function (req, res) {
+  return res.status(200).send("Welcome in the API IMDb Teste Backend");
 });
 routes.use("/auth", auth);
 routes.use("/user", user);
+routes.use("/movie", movie);
 
 export default routes;
---
+++ src/routes/movie.ts
@@ -0,0 +1,56 @@
+import { Router } from "express";
+import MovieController from "../controllers/MovieController";
+import { checkJwt } from "../middlewares/checkJwt";
+import { checkRole } from "../middlewares/checkRole";
+
+const router = Router();
+
+// Get all users
+router.get("/", [checkJwt, checkRole(["ADMIN"])], MovieController.listAll);
+
+// Get one user
+router.get(
+  "/:id([0-9]+)",
+  [checkJwt, checkRole(["ADMIN"])],
+  MovieController.getOneById
+);
+
+// Create a new user
+router.post("/", [checkJwt, checkRole(["ADMIN"])], MovieController.newMovie);
+
+// Edit one user
+router.put(
+  "/:id([0-9]+)",
+  [checkJwt, checkRole(["ADMIN"])],
+  MovieController.editMovie
+);
+
+// Edit one user
+router.patch(
+  "/:id([0-9]+)",
+  [checkJwt, checkRole(["ADMIN"])],
+  MovieController.editMovie
+);
+
+// Logically deletes a user
+router.delete(
+  "/:id([0-9]+)",
+  [checkJwt, checkRole(["ADMIN"])],
+  MovieController.deleteMovie
+);
+
+// Delete user by id forever
+router.delete(
+  "/:id([0-9]+)/forever",
+  [checkJwt, checkRole(["ADMIN"])],
+  MovieController.deleteMovieForever
+);
+
+// Recover a logically deleted user
+router.post(
+  "/:id([0-9]+)/recover",
+  [checkJwt, checkRole(["ADMIN"])],
+  MovieController.recover
+);
+
+export default router;
