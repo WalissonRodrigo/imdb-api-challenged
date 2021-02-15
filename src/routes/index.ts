b8e747d183da8b44cd825c3d86e75b781ab79205
Walisson Rodrigo <walisson.rodrigo@turbi.com.br>
2021-02-15 04:04:24 -0300

    remove route not used.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,10 @@
 import { Router } from "express";
 import auth from "./auth";
 import user from "./user";
-import movie from './movie';
+import movie from "./movie";
 
 const routes = Router();
-// response default to not return error on server.
-routes.get("/", [], function (req, res) {
-  return res.status(200).send("Welcome in the API IMDb Teste Backend");
-});
+
 routes.use("/auth", auth);
 routes.use("/user", user);
 routes.use("/movie", movie);
